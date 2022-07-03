98c895f67c9ecd8bae79ffa381212b2ba7fcfb53
BobbyWch <783813951@qq.com>
2022-07-03 12:06:07 +0800

    first

---
+++ src/bobby/Hook.java
@@ -0,0 +1,23 @@
+package bobby;
+
+import net.minecraft.client.Minecraft;
+
+public class Hook {
+    /**
+     * Invoke when start game.
+     */
+    public static void start(){
+
+    }
+
+    /**
+     * Invoke when stop game.
+     */
+    public static void stop(){}
+
+    /**
+     * Invoke when render.
+     */
+    public static void r() {
+    }
+}
